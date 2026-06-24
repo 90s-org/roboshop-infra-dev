96fb85228b8318b3b67eec6c8e5822bb117e54dc
joindevopscloud <info@joindevops.com>
2026-06-24 13:39:59 +0530

    terraform

--- 50-catalogue/variables.tf
+++ 50-catalogue/variables.tf
@@ -7,5 +7,13 @@ variable "environment" {
 }
 
 variable "app_version" {
-    
+    default = "v3"
+}
+
+variable "zone_id" {
+    default = "Z07086101C1CVP7AT2UK4"
+}
+
+variable "domain_name" {
+    default = "daws90s.shop"
 }
