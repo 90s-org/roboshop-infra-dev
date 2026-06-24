1974f4912223525a9778d7a36f6b9c971b73d3ee
joindevopscloud <info@joindevops.com>
2026-06-24 12:20:07 +0530

    terraform

--- 40-database/variables.tf
+++ 40-database/variables.tf
@@ -8,3 +8,11 @@ variable "environment" {
 
 variable "mysql_root_password" {
 }
+
+variable "zone_id" {
+    default = "Z07086101C1CVP7AT2UK4"
+}
+
+variable "domain_name" {
+    default = "daws90s.shop"
+}
