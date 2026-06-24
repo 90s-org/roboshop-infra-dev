a9a8f8493ff159c361428df5c848429abdc4cb66
joindevopscloud <info@joindevops.com>
2026-06-24 12:02:20 +0530

    terraform

--- 50-catalogue/locals.tf
+++ 50-catalogue/locals.tf
@@ -6,9 +6,6 @@ locals {
         Terraform = "true"
     }
     # public subnet in 1a AZ
-    public_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
-    redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
-    mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-    rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
 }
