584b6b52f030c6175491a8dda5f89afe2e3ab05d
joindevopscloud <info@joindevops.com>
2026-06-24 11:08:58 +0530

    terraform

---
+++ 30-bastion/main.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "bastion" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.public_subnet_id
+  vpc_security_group_ids = [local.bastion_sg_id]
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+  user_data = templatefile("bastion.sh.tftpl", {
+    partition_number = 4
+    extend_size_gb   = 30
+  })
+
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+    # EBS volume tags
+    tags = merge(
+      {
+          Name = "${var.project}-${var.environment}-bastion"
+      },
+    local.common_tags
+    )
+  }
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-bastion"
+    },
+    local.common_tags
+  )
+}
---
+++ 50-catalogue/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dnf install ansible -y
+mkdir -p /var/log/roboshop/
+chown -R ec2-user:ec2user /var/log/roboshop/
+chown -R 755 /var/log/roboshop/
+touch /var/log/roboshop/ansible.log
+
+cd /home/ec2-user
+git clone https://github.com/90s-org/roboshop-ansible-v3.git
+
+cd roboshop-ansible-v3
+git pull
+ansible-playbook -e component=$1 -e environment=$2 roboshop.yaml
