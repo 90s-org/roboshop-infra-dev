7fe0ed6453b7bac54d8d8fd2032fc46c0d589090
joindevopscloud <info@joindevops.com>
2026-06-27 16:36:37 +0530

    oil and karam added

--- 70-components/variables.tf
+++ 70-components/variables.tf
@@ -1,10 +1,10 @@
 variable "components" {
     default = {
         # backend components are attaching to backend ALB
-        /* catalogue = {
+        catalogue = {
             rule_priority = 10
         }
-        user = {
+        /* user = {
             rule_priority = 20
         }
         cart = {
